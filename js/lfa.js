9cfed99ffad981c00857357fecfaef7b58223544
Other Realm <the@otherrealm.org>
2020-03-26 11:37:11 -0400

    Working on who you love info

--- js/lfa.js
+++ js/lfa.js
@@ -14,11 +14,12 @@
 /* global tagify */
 (function () {
 	var interests = ["Science", "DIY", "Programming", "Inventing"];
-	var problems = ['COVID-19'];
+	var gender = ['Female', 'Male', 'Trans'];
 	var inpInterests = document.querySelector('input[name="interests"]');
-	var inpProblems = document.querySelector('input[name="problems"]');
+	var inpGender = document.querySelector('input[name="problems"]');
 	var interestsFun = boxToFill(inpInterests, interests);
-	var interestsProb = boxToFill(inpProblems, problems);
+	var interestsProb = boxToFill(inpGender, gender);
+//	var obAbVal = $('#objectives,#about');
 	function boxToFill(input, wl) {
 		tagify = new Tagify(input, {
 			whitelist: wl,
@@ -36,7 +37,38 @@
 		console.log(form);
 		$.post('../../love-from-afar-ss/love-from-afar-ss.php', form, function (d) {
 			console.log(d);
+//			$('#results').html(d.interests[0].value);
 		});
 		f.preventDefault();
+	});
+	$("input[name|='be']").on('ready DOMContentLoaded change', function (d) {
+		if ($(this).is('#beyourself:checked')) {
+			$('#objectives,#about').show(function () {
+				$(this).prop('required', true);
+			});
+			$("label[for='objectives'],label[for='about']").show();
+			console.log($(this));
+		}
+		if ($(this).is('#bepredesigned:checked')) {
+			$('#objectives,#about').hide(function () {
+				$(this).prop('required', false);
+			});
+			$("label[for='objectives'],label[for='about']").hide();
+			console.log($(this));
+		}
+		if ($(this).is('#becreatedbyyou:checked')) {
+			$('#objectives,#about').show(function () {
+				$(this).prop('required', true);
+			});
+			$("label[for='objectives'],label[for='about']").show();
+			console.log($(this));
+		}
+		if ($(this).is('#bemultiple:checked')) {
+			$('#objectives,#about').show(function () {
+				$(this).prop('required', true);
+			});
+			$("label[for='objectives'],label[for='about']").show();
+			console.log($(this));
+		}
 	});
 })(jQuery);
