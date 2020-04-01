f7ade3d4c673884a20cbe6e46c6fa6136cf7afb3
Other Realm <the@otherrealm.org>
2020-04-01 12:06:28 -0400

    Working on profile pages

---
+++ .history/js/lfa_20200331152524.js
@@ -0,0 +1,37 @@
+/*
+ Created		: Mar 18, 2020, 6:44:36 PM
+ Author		: Aaron E-J <the at otherrealm.org>
+ Copyright(C): 2020 Other Realm LLC
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the latest version of the GNU Affero General Public License as published by
+ the Free Software Foundation, using at least version 3.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details: <http://www.gnu.org/licenses/>.
+ */
+/* global tagify, CryptoJS 
+
+		title: "Love From Afar",
+*/
+Vue.component('be', {
+	props: ['tobe'],
+	template: '<div><input type="radio" :id="tobe.id" name="be" :value="tobe.id" required> <label :for="tobe.id">{{tobe.text}}</label></div>'});
+var app = new Vue({
+	el: '#app',
+	data: {
+		title: "Love From Afar",
+		be: [
+			{ id: 'yourself', text: "Yourself" },
+			{ id: 'predesigned', text: "Pedesigned" },
+			{ id: 'createdbyyou', text: "Created by you" },
+			{ id: 'multiple', text: "Multiple" }
+		]
+	}
+});
+(function ($) {
+	$('input').change(function (d) {
+		console.log($(this).val());
+	});
+})(jQuery);
